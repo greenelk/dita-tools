06a3260c5909c8c40f847e3d6b2829f366c8648c
greenelk <greenelk@gmail.com>
2015-04-21 16:12:17 +0100

    setting up build infrastructure

---
+++ src/main/java/org/greenelk/ditatools/LinkCheck.java
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the greenelk/dita-tools GitHub project.
+ * See the accompanying license.txt file for applicable licenses.
+ */
+package org.greenelk.ditatools;
+
+public class LinkCheck {
+
+	public static void main(String[] args) {
+		System.out.println("LinkCheck coming soon...");
+
+	}
+
+}
